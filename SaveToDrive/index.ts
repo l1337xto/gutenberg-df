b282554ae977b2d5dd47689f2e67a0ec32f26b1f
Saurabh Surana <saurabh.surana@crst.com>
2022-12-12 15:03:36 +0530

    Change to Netherite

--- SaveToDrive/index.ts
+++ SaveToDrive/index.ts
@@ -9,15 +9,16 @@ const gc = new Storage({
 const bucket = gc.bucket('gutenberg-analysis-uga');
 
 const activityFunction: AzureFunction = async function (context: Context): Promise<string> {
-    context.log(context.bindingData?.instanceId,":::SaveToDrive: Initializing processing.")
+    context.log(context.bindingData?.instanceId,":::SaveToDrive: Initializing Saving to Cloud.")
     const bookPath:string = context.bindingData.bookPath;
     const fileName:string = bookPath.split("/").pop();
     const data = context.bindings.name.bookSummary.toString();
-    const file = bucket.file(fileName);
+    const file = bucket.file(fileName+String(new Date().getTime()));
     const passthroughStream = new stream.PassThrough();
     passthroughStream.write(data);
     passthroughStream.end();
     await streamFileUpload(passthroughStream,file).catch((_err) => {});
+    context.log(context.bindingData?.instanceId,":::SaveToDrive: Finished Saving to Cloud.")
     return `File ${fileName} uploaded to cloud.`;
 };
 async function streamFileUpload(passthroughStream, file) {
